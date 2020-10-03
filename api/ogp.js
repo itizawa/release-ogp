135ca3eb84ac69a06569ebeef34c199866b9af9c
itizawa <itizawa@weseek.co.jp>
2020-10-03 16:47:26 +0900

    customize

--- api/ogp.js
+++ api/ogp.js
@@ -27,10 +27,11 @@ export default async (req, res) => {
     const TEXT_LINE_MARGIN_SIZE = 16;
     const TEXT_MARGIN_X = 68;
 
-    const FONT_FAMILY = "shirokuma-regular";
-    const FONT_PATH = path.join(__dirname, "..", "fonts", "shirokuma-regular.ttf");
+    const FONT_FAMILY = "logotypejp_corpmin";
+    const FONT_PATH = path.join(__dirname, "..", "fonts", "logotypejp_corpmin.ttf");
 
     const BACKGROUND_IMAGE_PATH = path.join(__dirname, "..", "images", "ogp-background.jpg");
+
     registerFont(FONT_PATH, { family: FONT_FAMILY });
     const canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
     const context = canvas.getContext("2d");
