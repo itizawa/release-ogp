c016a0cc4864b1a144423eeb0b3d3bf9cdca1bad
itizawa <itizawa@weseek.co.jp>
2020-10-03 16:53:22 +0900

    adjust

--- api/ogp.js
+++ api/ogp.js
@@ -30,7 +30,7 @@ export default async (req, res) => {
     const FONT_FAMILY = "logotypejp_corpmin";
     const FONT_PATH = path.join(__dirname, "..", "fonts", "logotypejp_corpmin.ttf");
 
-    const BACKGROUND_IMAGE_PATH = path.join(__dirname, "..", "images", "ogp-background.jpg");
+    const BACKGROUND_IMAGE_PATH = path.join(__dirname, "..", "images", "ogp-background.png");
 
     registerFont(FONT_PATH, { family: FONT_FAMILY });
     const canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
